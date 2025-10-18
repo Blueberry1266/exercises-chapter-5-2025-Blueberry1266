515eedd37a8eb34efb2dc7028905b06b738257e7
Zijun Xu <zijun05xx@126.com>
2025-10-18 23:03:24 +0100

    Exercise 5 Version 3.0 Complete All

--- adt_examples/fibonacci.py
+++ adt_examples/fibonacci.py
@@ -5,3 +5,12 @@ class Fib:
     def __init__(self):
         self.current = 1
         self.following = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current, self.following = (
+            self.following, self.current + self.following
+        )
+        return self.current
